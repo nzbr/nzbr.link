3b770ef7397fdb6785bacf1f48db17a6388281d1
nzbr <mail@nzbr.de>
2022-07-03 03:14:14 +0200

    adjust nix build

---
+++ flake.nix
@@ -0,0 +1,72 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils } @ inputs:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      rec {
+
+        devShells = {
+          default = pkgs.mkShell {
+            buildInputs = with pkgs; [
+              nixpkgs-fmt
+              nodejs-16_x
+              yarn
+              packages.prefetch-docker
+            ];
+          };
+        };
+
+        packages = rec {
+          nzbr-dot-link =
+            pkgs.mkYarnPackage {
+              name = "nzbr.link";
+              src = ./.;
+              buildPhase = ''
+                export HOME=$PWD
+                ln -sfT $node_modules deps/$pname/node_modules
+                yarn --offline build
+              '';
+              installPhase = ''
+                cp -r deps/$pname/dist $out
+              '';
+              distPhase = "true";
+            };
+
+          prefetch-docker = pkgs.writeShellScriptBin "prefetch-docker" ''
+            ${pkgs.nix-prefetch-docker}/bin/nix-prefetch-docker nginx alpine > nginx.nix
+          '';
+
+          docker-image = pkgs.dockerTools.buildLayeredImage {
+            name = "nzbr.link";
+            tag = "latest";
+
+            fromImage = pkgs.dockerTools.pullImage (import ./nginx.nix);
+
+            contents = [
+              (pkgs.substituteAll {
+                name = "nginx.conf";
+                src = ./nginx.conf;
+                dir = "etc/nginx";
+                root = nzbr-dot-link;
+              })
+            ];
+
+            config.Cmd = [ "nginx" "-g" "daemon off;" ];
+          };
+        };
+
+        checks = {
+          nixpkgs-fmt = pkgs.runCommand "check-nix-format" { } ''
+            ${pkgs.nixpkgs-fmt}/bin/nixpkgs-fmt --check ${./.}
+            mkdir $out #sucess
+          '';
+        };
+
+      });
+}
---
+++ nginx.nix
@@ -0,0 +1,7 @@
+{
+  imageName = "nginx";
+  imageDigest = "sha256:8e38930f0390cbd79b2d1528405fb17edcda5f4a30875ecf338ebaa598dc994e";
+  sha256 = "0s822n6qm1g5znv6dsxf0adjsknyag1bjzsd68cw3n4hllgbqq9m";
+  finalImageName = "nginx";
+  finalImageTag = "alpine";
+}
