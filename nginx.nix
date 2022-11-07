88ba947769365350c2b5bc76f3d077526fc1da9b
nzbr <mail@nzbr.de>
2022-11-07 08:59:37 +0100

    update nginx image

--- nginx.nix
+++ nginx.nix
@@ -1,7 +1,7 @@
 {
   imageName = "nginx";
-  imageDigest = "sha256:87fb6f4040ffd52dd616f360b8520ed4482930ea75417182ad3f76c4aaadf24f";
-  sha256 = "1f23bjc6ff1kbd8wly8dc4nn341lf9wlcsg3ap0mwnd6njikbmgj";
+  imageDigest = "sha256:2452715dd322b3273419652b7721b64aa60305f606ef7a674ae28b6f12d155a3";
+  sha256 = "1fc2q46lmspkczgmlf9nxxyc6sa59bzyvrljrqkprk8670i23r8x";
   finalImageName = "nginx";
   finalImageTag = "alpine";
 }
