b705bc2c45a54250a8840c75fedbb5d3a7a06a97
nzbr <mail@nzbr.de>
2022-12-29 05:37:49 +0100

    fix warnings

--- docker.nix
+++ docker.nix
@@ -7,6 +7,11 @@ dockerTools.buildLayeredImage {
   name = "nzbr.link";
   tag = "latest";
 
+  # create a path for ASP.NET to store its data protection key
+  fakeRootCommands = ''
+    mkdir tmp
+  '';
+
   config = {
     Cmd = [ "${dotnet-aspnetcore_7}/bin/dotnet" "backend.dll" "--urls=http://0.0.0.0:80/" ];
     WorkingDir = backend;
