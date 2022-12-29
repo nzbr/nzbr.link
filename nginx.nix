c7688a48b1dbe6d1041438a7c0794b9c277ba062
nzbr <mail@nzbr.de>
2022-12-29 05:41:02 +0100

    remove nginx related files; fix ci

--- nginx.nix
+++
@@ -1,7 +0,0 @@
-{
-  imageName = "nginx";
-  imageDigest = "sha256:dd8a054d7ef030e94a6449783605d6c306c1f69c10c2fa06b66a030e0d1db793";
-  sha256 = "1wljwzcdfw5yid7w3dq5jg2r3p5bpras0f76i8s0vsw7vspqc13y";
-  finalImageName = "nginx";
-  finalImageTag = "alpine";
-}
