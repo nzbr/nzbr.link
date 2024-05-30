da2c22725c9987f9eacc5a7a999b927967a5e852
nzbr <mail@nzbr.de>
2024-05-30 20:50:37 +0200

    add simpleicons

--- frontend/hbs.config.js
+++ frontend/hbs.config.js
@@ -4,23 +4,43 @@ const yaml = require('js-yaml');
 module.exports = {
   buttons: yaml.load(fs.readFileSync('./buttons.yaml', 'utf8')).map(button => ({
     ...button,
-    icon: ((icon) => {
-      if (icon.startsWith('fa:')) {
-        for (const set of [ 'regular', 'brands', 'solid' ]) {
-          const candidate = `/node_modules/@fortawesome/fontawesome-free/svgs/${set}/${button.icon.substring(3)}.svg`;
-          if (fs.existsSync(`.${candidate}`)) {
-            return candidate;
+    icon: ((iconstring) => {
+      const [set, icon] = iconstring.split(':');
+
+      switch (set) {
+        case 'si': {
+          const candidate = `/node_modules/simple-icons/icons/${icon}.svg`;
+          if (!fs.existsSync(`.${candidate}`)) {
+            throw new Error(`Could not find ${icon} in Simple Icons`);
           }
+          return candidate;
         }
+        case 'fa': {
+          for (const set of ['regular', 'brands', 'solid']) {
+            const candidate = `/node_modules/@fortawesome/fontawesome-free/svgs/${set}/${icon}.svg`;
+            if (fs.existsSync(`.${candidate}`)) {
+              return candidate;
+            }
+          }
 
-        throw new Error(`Could not find icon ${button.icon}`)
+          throw new Error(`Could not find ${icon} in Font Awesome`);
+        }
+        case 'mdi': {
+          const candidate = `/node_modules/@mdi/svg/svg/${icon}.svg`;
+          if (!fs.existsSync(`.${candidate}`)) {
+            throw new Error(`Could not find ${icon} in Material Design Icons`);
+          }
+          return candidate;
+        }
+        default: {
+          const icondir = "src/img/icons"
+          const candidate = `/${icondir}/${iconstring}`;
+          if (!fs.existsSync(`.${candidate}`)) {
+            throw new Error(`Could not find ${iconstring} in ${icondir}`);
+          }
+          return candidate;
+        }
       }
-
-      if (icon.startsWith('mdi:')) {
-        return `/node_modules/@mdi/svg/svg/${button.icon.substring(4)}.svg`
-      }
-
-      return `img/icons/${button.icon}`;
     })(button.icon),
   })),
   address: 'TmljbyBKYW5zZW4KYy9vIEJsb2NrIFNlcnZpY2VzClN0dXR0Z2FydGVyIFN0ci4gMTA2CjcwNzM2IEZlbGxiYWNo',
